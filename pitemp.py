4bea7ce5b863b0027163762dfbbf493f87c04401
Jeffrey Davidson <jedavids@cisco.com>
2019-05-04 16:00:58 -0400

    Add sensor cmd line option

--- pitemp.py
+++ pitemp.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+import argparse
 import os
 import glob
 import time
@@ -11,17 +12,20 @@ os.system('modprobe w1-therm')
 
 starttime = None
 base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+device_folders = glob.glob(base_dir + '28*')
+#device_file = device_folder + '/w1_slave'
 
-def read_temp_raw():
+print(glob.glob(base_dir + '28*'))
+
+def read_temp_raw(sensor):
+    device_file = device_folder[sensor] + '/w1_slave'
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
     return lines
 
-def read_temp():
-    lines = read_temp_raw()
+def read_temp(sensor):
+    lines = read_temp_raw(sensor)
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
         lines = read_temp_raw()
@@ -44,8 +48,25 @@ def time_offset():
         starttime = get_seconds()
     return get_seconds() - starttime
 
+def ParseCmdLine():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-s", "--sensor", help="Number value of the sensor to use (0-3)", default=0)
+    args = parser.parse_args()
+    if args.address not in range(3):
+        print("Invalid sensor")
+        return None
+    return args
 
-for i in range(360):
-    print("{:>3}, {:.3f}".format(time_offset(), read_temp()))
-    sys.stdout.flush()
-    time.sleep(5)
+
+def main():
+
+    args = ParseCmdLine()
+
+    for i in range(360):
+        print("{:>3}, {:.3f}".format(time_offset(), read_temp(args.sensor)))
+        sys.stdout.flush()
+        time.sleep(5)
+
+
+if __name__=='__main__':
+    sys.exit(main())
